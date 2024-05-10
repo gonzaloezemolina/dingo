4e106b27717a8367857ddf39acaca5bfe5061135
gonzaloezemolina <gonzaloezemolina@gmail.com>
2024-05-10 16:15:42 -0300

    Dingo first saved. 0.01

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import config from "./config/config.js";
+import express from 'express';
+import viewRouter from "./router/views.router.js";
+import Handlebars from "handlebars";
+import { allowInsecurePrototypeAccess } from "@handlebars/allow-prototype-access";
+import ExpressHandlebars  from "express-handlebars";
+import __dirname from "./utils.js";
+
+export const app = express();
+
+const PORT = config.app.PORT;
+
+const server = app.listen(PORT, () =>{
+    console.log(`Server HTTP is listening on PORT ${server.address().port}`);
+})
+
+server.on("Error", error => console.log(`Error en el servidor ${error}`));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`))
+
+
+//Handlebars
+  app.engine('handlebars',ExpressHandlebars.engine({handlebars: allowInsecurePrototypeAccess(Handlebars),}));
+  app.set("view engine", "handlebars" );
+  app.set("views", __dirname+ "/views");
+
+
+app.use("/", viewRouter);
